60e6d524325de04db77d33d61bd26c9ab11bd572
Carlos Pantelides <cpantel@github.com>
2021-05-23 12:08:20 -0300

    working with debug

--- mbed-os/main.cpp
+++ mbed-os/main.cpp
@@ -1,19 +1,36 @@
 #include "mbed.h"
 #include "USBMouse.h"
 #include "arm_book_lib.h"
-#include "Debouncer.h"
-#include "Encoder.h"
-#include "MouseClick.h"
+
+#include "arm_math.h"
+
+#define STEPS 30
+#define DELAY 0.05
+
+
+    Serial uartUsb(USBTX, USBRX);
+int16_t fixStep(int16_t step) {
+    if (step == STEPS) {
+      uartUsb.printf("  ==  STEPS \r\n");
+      return 0;
+    }
+    if (step > STEPS) {
+      uartUsb.printf("  >  STEPS \r\n");
+      return ( step - STEPS);
+    }
+    if (step < 0) {
+      uartUsb.printf("  <  STEPS \r\n");
+      return (step + STEPS);
+    }
+    return step;
+}
 
 
 
 int main() {
-    Serial uartUsb(USBTX, USBRX);
 
-    uartUsb.printf("\r\nSlither USB version 0.1.229\r\n");
 
-    EventQueue queue;
-    Thread eventThread;
+    uartUsb.printf("\r\nTesting usbmouse vs host\r\n");
 
     uartUsb.printf("Waiting for USB connection...");
 
@@ -21,55 +38,69 @@ int main() {
 
     uartUsb.printf(" connected!\r\n");
 
-    MouseClick leftButton(&mouse, MOUSE_LEFT);
-    Encoder encoder(&mouse);
-
-    Debouncer<MouseClick> leftButtonDebouncer(BUTTON1, &queue, &leftButton, &MouseClick::press, &MouseClick::release); // BUTTON1/ PC_10
-    Debouncer<Encoder> clkEncoderDebouncer(PC_8, &queue, &encoder, &Encoder::clkUp, &Encoder::clkDown);
-    Debouncer<Encoder> dtEncoderDebouncer(PC_9, &queue, &encoder, &Encoder::dtUp, &Encoder::dtDown);
-    encoder.setSerial(&uartUsb);
-    eventThread.start(callback(&queue, &EventQueue::dispatch_forever));
-
-/*
-   #define STEPS 30
-   int step = 0;
    float32_t moves[STEPS][2];
-   float32_t radian =
-      (( STEPS -1) * ( 360 / STEPS) / 360.0 ) * ( 2.0 *  3.14159);
+   float32_t radius = 100;
+   int16_t step = 0;
+
+   float32_t radian = (( STEPS -1) * ( 360 / STEPS) / 360.0 ) * ( 2.0 *  3.14159);
 
    float32_t prev_x = cos(radian);
    float32_t prev_y = sin(radian);
 
    for (step = 0; step < STEPS; ++step) {
-     float32_t radian = (step * ( 360 / STEPS) / 360.0 ) * ( 2.0 *  3.14159);
-     float32_t next_x = cos(radian);
-     float32_t next_y = sin(radian);
+      float32_t radian = (step * ( 360 / STEPS) / 360.0 ) * ( 2.0 *  3.14159);
+      float32_t next_x = cos(radian);
+      float32_t next_y = sin(radian);
 
-     moves[step][0] =  cos(radian) - prev_x;
-     moves[step][1] =  sin(radian) - prev_y;
-     prev_x = next_x;
-     prev_y = next_y;
-
-     uartUsb.printf("Step: %d x: %.4f y: %.4f\r\n",
-         step, moves[step][0], moves[step][1]);
+      moves[step][0] =  cos(radian) - prev_x;
+      moves[step][1] =  sin(radian) - prev_y;
+      prev_x = next_x;
+      prev_y = next_y;
    }
 
-   int radius = 100;
-*/
-    wait(osWaitForever);
-/*   while (1) {
-       for (step = 0; step < (STEPS * 2); ++step) {
-          mouse.move(moves[step%STEPS][0] * radius,
-                     moves[step%STEPS][1] * radius);
-          wait(2.0);
-       }
-       wait(2.0);
-       for (step = ((STEPS * 2) - 1); step >= 0; --step) {
-          mouse.move(moves[step%STEPS][0] * radius * -1 ,
-                     moves[step%STEPS][1] * radius * -1 );
-          wait(2.0);
-       }
-    }
-*/
+   uartUsb.printf("--------------------------------------------------\r\n");
+   for (step = 0; step < STEPS; ++step) {
+        uartUsb.printf("step : %d delta x : %.4f delta y : %.4f\r\n", step, moves[step][0], moves[step][1]);
+   }
+   uartUsb.printf("--------------------------------------------------\r\n");
+   int moveDuration[30] = {4,5,6,7,8,12,3,32,7,32,53,47,18,34,43,57,32,42,48,24,73,24,21,34,24,87,12,10,14,90};
+
+   int pos = 0;
+   int idx = 0;
+   step = 0;
+   mouse.press(MOUSE_LEFT);
+   while (idx < 30)  {
+      for (pos = 0; pos < moveDuration[idx]; ++pos) {
+        step = fixStep(step);
+        uartUsb.printf("+ pos : %d duration: %d step : %d delta x : %.4f delta y : %.4f\r\n", pos, moveDuration[idx], step, moves[step][0], moves[step][1]);
+        mouse.move(moves[step][0] * radius * 1, moves[step][1] * radius * 1);
+        wait(DELAY);
+
+        step =fixStep(step +1);
+      }
+
+        step =fixStep(step -1);
+      ++idx;
+      for (pos = 0;  pos < moveDuration[idx]; ++pos) {
+
+        uartUsb.printf("- pos : %d duration: %d step : %d delta x : %.4f delta y : %.4f\r\n", pos, moveDuration[idx], step, moves[step][0], moves[step][1]);
+        mouse.move(moves[step][0] * radius * -1, moves[step][1] * radius * -1);
+        wait(DELAY);
+
+
+        step =fixStep(step -1);
+     }
+
+        step =fixStep(step +1);
+      ++idx;
+    wait(0.5);
+
+
+   }
+   mouse.release(MOUSE_LEFT);
+  while(1) {}
+
+
+
 
 }
